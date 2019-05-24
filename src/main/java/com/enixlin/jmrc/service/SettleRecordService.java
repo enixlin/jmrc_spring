a5d0290b73b9ee2c0bce63238e5add78b064ef0d
enixlin <enixlin@126.com>
2019-05-24 16:05:14 +0800

    add ods tf query

--- src/main/java/com/enixlin/jmrc/service/SettleRecordService.java
+++ src/main/java/com/enixlin/jmrc/service/SettleRecordService.java
@@ -1,7 +1,13 @@
 package com.enixlin.jmrc.service;
 
+import java.util.ArrayList;
+
+import com.enixlin.jmrc.entity.Performance;
 import com.enixlin.jmrc.entity.SettleRecord;
 
 public interface SettleRecordService extends BaseService<SettleRecord> {
 
+	public ArrayList<Performance> getBranchPerformance();
+	
+	
 }
