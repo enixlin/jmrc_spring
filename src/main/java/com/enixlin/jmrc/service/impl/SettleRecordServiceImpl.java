e407e7eacdc46cbd974621cdf6ab803ec599fbe3
enixlin <enixlin@126.com>
2019-05-28 19:33:45 +0800

    f

--- src/main/java/com/enixlin/jmrc/service/impl/SettleRecordServiceImpl.java
+++ src/main/java/com/enixlin/jmrc/service/impl/SettleRecordServiceImpl.java
@@ -1,8 +1,11 @@
 package com.enixlin.jmrc.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.enixlin.jmrc.entity.Performance;
 import com.enixlin.jmrc.entity.SettleRecord;
 import com.enixlin.jmrc.mapper.SettleRecordMapper;
 import com.enixlin.jmrc.service.SettleRecordService;
@@ -15,5 +18,11 @@ public class SettleRecordServiceImpl extends BaseServiceImpl<SettleRecord> imple
 	public SettleRecord add(SettleRecord reocrd) {
 		settleRecordMapper.insert(reocrd);
 		return reocrd;
+	}
+
+	@Override
+	public ArrayList<Performance> getBranchPerformance() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
